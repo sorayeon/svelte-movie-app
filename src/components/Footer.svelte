9fb5b5f28c01437b309c9b96382b504ebeeaafea
jihwa <jihwan77@gmail.com>
2020-12-19 21:44:02 +0900

    Create Project

---
+++ src/components/Footer.svelte
@@ -0,0 +1,29 @@
+<footer>
+    <div>
+        <a href="#none">
+            GitHub Repo.
+        </a>
+    </div>
+    <div>
+        <a href="">
+            @{new Date().getFullYear()} HEROPY
+        </a>
+    </div>
+</footer>
+
+<style lang="scss">
+    footer {
+        padding: 70px;
+        text-align: center;
+        div {
+            a {
+                color: $color--white-10;
+                text-decoration: none;
+
+                &:hover {
+                    text-decoration: underline;
+                }
+            }
+        }
+    }
+</style>
